3f03d1da6ea957808ca0c05c7739450eafc27b94
rbwls6656@gmail.com <rbwls6656@gmail.com>
2021-12-26 23:41:25 +0900

    init code

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,9 +7,8 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int steps = Integer.parseInt(bufferedReader.readLine().trim());
-        String path = bufferedReader.readLine();
-        int result = Result.countingValleys(steps, path);
+        // write code here
+        int result = 0;
 
         // do not change
         bufferedWriter.write(String.valueOf(result));
@@ -20,33 +19,3 @@ public class Solution {
 }
 
 
-class Result {
-    /*
-     * Complete the 'countingValleys' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER steps
-     *  2. STRING path
-     */
-
-    public static int countingValleys(int steps, String path) {
-        // Write your code here
-        int valleyCount = 0;
-
-        int heightLevel = 0;
-        for (int i = 0; i < steps; i++) {
-            if (path.charAt(i) == 'U') {
-                heightLevel++;
-            }
-            else if (path.charAt(i) == 'D'){
-                heightLevel--;
-            }
-
-            if (heightLevel == 0 && path.charAt(i) == 'U') { // valley가 성립하려면 이번에 해석한 paht.chatAt(i)이 U이면서 heightLevel을 0(해수면)으로 만들어야 한다.
-                valleyCount++;
-            }
-        }
-        return valleyCount;
-    }
-}
