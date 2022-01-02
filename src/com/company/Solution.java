9809463c23549287f8be184650d5dbc3be0a7bc6
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-02 10:41:38 +0900

    salesByMatch
    https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,6 +1,13 @@
 package com.company;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         // do not change
@@ -8,7 +15,13 @@ public class Solution {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // write code here
-        int result = 0;
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
 
         // do not change
         bufferedWriter.write(String.valueOf(result));
@@ -19,3 +32,36 @@ public class Solution {
 }
 
 
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+        Map<Integer, Integer> hmap = new HashMap<>();
+        for(Integer sockNumber : ar) {
+            if(hmap.get(sockNumber) != null) {
+                hmap.put(sockNumber, hmap.get(sockNumber) + 1);
+            }
+            else {
+                hmap.put(sockNumber, 1);
+            }
+        }
+
+        int res = 0;
+        for(Integer sockNumber : hmap.keySet()) {
+            Integer sockAppearCount = hmap.get(sockNumber);
+            res = res + sockAppearCount / 2;
+        }
+
+        return res;
+    }
+
+}
