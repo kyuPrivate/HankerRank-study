54f3140d70f98bd69dd722a8577b18a45bb0bf1b
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-02 10:00:12 +0900

    jumping on clouds
    https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,6 +1,10 @@
 package com.company;
 
 import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         // do not change
@@ -8,7 +12,12 @@ public class Solution {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // write code here
-        int result = 0;
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+        int result = Result.jumpingOnClouds(c);
 
         // do not change
         bufferedWriter.write(String.valueOf(result));
@@ -19,3 +28,37 @@ public class Solution {
 }
 
 
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Write your code here
+        int res = 0;
+        int index = 0;
+        int lastIndex = c.size() - 1;
+
+        return calc(res, index, lastIndex, c);
+    }
+
+    private static int calc (int res, int index, int lastIndex, List<Integer> c) {
+        if (index + 1 >= lastIndex || index + 2 >= lastIndex) {
+            return res + 1;
+        }
+        if (c.get(index + 2) == 0) {
+            return calc(res + 1, index + 2, lastIndex, c);
+        }
+        if (c.get(index + 1) == 0) {
+            return calc(res + 1, index + 1, lastIndex, c);
+        }
+        return 0;
+    }
+
+
+
+}
