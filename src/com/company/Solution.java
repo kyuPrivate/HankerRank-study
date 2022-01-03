a32847931e6e5ed72270056b2fbee33f024aec84
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-03 23:32:43 +0900

    Left Rotation
    https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,6 +1,17 @@
 package com.company;
 
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         // do not change
@@ -8,7 +19,15 @@ public class Solution {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // write code here
-        int result = 0;
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+        int n = Integer.parseInt(firstMultipleInput[0]);
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.rotLeft(a, d);
 
         // do not change
         bufferedWriter.write(String.valueOf(result));
@@ -18,4 +37,25 @@ public class Solution {
     }
 }
 
+class Result {
 
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        // Write your code here
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < a.size(); i++) {
+            int index = (i + d) % a.size();
+            result.add(a.get(index));
+        }
+        return result;
+    }
+
+}
