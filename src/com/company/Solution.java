418dff3036d4dd22531c344bca63f6dbfb5f44fe
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-05 23:52:37 +0900

    Minimum Swaps 2
    https://www.hackerrank.com/challenges/minimum-swaps-2/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,21 +1,57 @@
 package com.company;
 
 import java.io.*;
+import java.util.Scanner;
+
 public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) throws IOException {
-        // do not change
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        // write code here
-        int result = 0;
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        // do not change
-        bufferedWriter.write(String.valueOf(result));
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
         bufferedWriter.newLine();
-        bufferedReader.close();
+
         bufferedWriter.close();
+
+        scanner.close();
     }
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int swapCount = 0;
+        for(int i = 0; i < arr.length; ) {
+            if (arr[i] != i + 1) {
+               swap(arr, i, arr[i] - 1);
+               swapCount++;
+            } else {
+                i++;
+            }
+        }
+        return swapCount;
+    }
+
+    static void swap (int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr [b];
+        arr[b] = temp;
+    }
+
 }
 
 
