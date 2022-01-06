56823c577d1bcf1e711a99ff8c6f1b9cfa7a8494
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-07 00:11:31 +0900

    Hash Tables: Ransom Note
    https://www.hackerrank.com/challenges/ctci-ransom-note/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,6 +1,18 @@
 package com.company;
 
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         // do not change
@@ -8,10 +20,19 @@ public class Solution {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // write code here
-        int result = 0;
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(firstMultipleInput[0]);
+        int n = Integer.parseInt(firstMultipleInput[1]);
+
+        List<String> magazine = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .collect(toList());
+
+        List<String> note = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .collect(toList());
+        Result.checkMagazine(magazine, note);
 
         // do not change
-        bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
         bufferedReader.close();
         bufferedWriter.close();
@@ -19,3 +40,38 @@ public class Solution {
 }
 
 
+class Result {
+
+    /*
+     * Complete the 'checkMagazine' function below.
+     *
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY magazine
+     *  2. STRING_ARRAY note
+     */
+
+    public static void checkMagazine(List<String> magazine, List<String> note)    {
+        // Write your code here
+        Map<String, Integer> wordCount = new HashMap<>();
+        for(String word : magazine) {
+            if(wordCount.get(word) == null) {
+                wordCount.put(word, 1);
+            } else {
+                wordCount.put(word, wordCount.get(word) + 1);
+            }
+        }
+
+        for (String word : note) {
+            if(wordCount.get(word) == null || wordCount.get(word) == 0) {
+                System.out.println("No");
+                return;
+            } else {
+                wordCount.put(word, wordCount.get(word) - 1);
+            }
+        }
+
+        System.out.println("Yes");
+        return;
+    }
+
+}
