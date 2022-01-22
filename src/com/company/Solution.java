efb2de47422686a6d6737a05380d69a80c207c4e
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-22 23:34:48 +0900

    Mark and Toys
    https://www.hackerrank.com/challenges/mark-and-toys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,6 +1,17 @@
 package com.company;
 
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 public class Solution {
     public static void main(String[] args) throws IOException {
         // do not change
@@ -8,7 +19,17 @@ public class Solution {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         // write code here
-        int result = 0;
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.maximumToys(prices, k);
 
         // do not change
         bufferedWriter.write(String.valueOf(result));
@@ -19,3 +40,30 @@ public class Solution {
 }
 
 
+class Result {
+
+    /*
+     * Complete the 'maximumToys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY prices
+     *  2. INTEGER k
+     */
+
+    public static int maximumToys(List<Integer> prices, int k) {
+        // Write your code here
+        Collections.sort(prices);
+        int count = 0;
+        for(int i = 0; i < prices.size(); i++) {
+            if(k - prices.get(i) < 0) {
+                break;
+            } else {
+                k -= prices.get(i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
