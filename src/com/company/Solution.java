550c3ff54c7881ed30e6dfc9fbeb32de8d78cd5e
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-22 23:27:43 +0900

    Sorting: Bubble Sort
    https://www.hackerrank.com/challenges/ctci-bubble-sort/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -1,17 +1,32 @@
 package com.company;
 
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException {
         // do not change
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // write code here
-        int result = 0;
 
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.countSwaps(a);
         // do not change
-        bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
         bufferedReader.close();
         bufferedWriter.close();
@@ -19,3 +34,36 @@ public class Solution {
 }
 
 
+class Result {
+
+    /*
+     * Complete the 'countSwaps' function below.
+     *
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static void countSwaps(List<Integer> a) {
+        // Write your code here
+
+        int swapCount = 0;
+        for(int i = a.size() - 1; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                if(a.get(j) > a.get(j + 1)) {
+                    swap(j, j + 1, a);
+                    swapCount++;
+                }
+            }
+        }
+
+        System.out.println("Array is sorted in " + swapCount + " swaps.");
+        System.out.println("First Element: " + a.get(0));
+        System.out.println("First Element: " + a.get(a.size() - 1));
+    }
+
+    public static void swap(int i, int j, List<Integer> a) {
+        Integer temp = a.get(i);
+        a.set(i, a.get(j));
+        a.set(j, temp);
+    }
+
+}
