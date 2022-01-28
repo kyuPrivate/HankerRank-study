8d0f37f4725ff4e38fbc0507b10c0da5e5772600
rbwls6656@gmail.com <rbwls6656@gmail.com>
2022-01-28 22:32:31 +0900

    Strings: Making Anagrams
    https://www.hackerrank.com/challenges/ctci-making-anagrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings

--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -7,15 +7,53 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // write code here
-        int result = 0;
 
-        // do not change
-        bufferedWriter.write(String.valueOf(result));
+        String a = bufferedReader.readLine();
+
+        String b = bufferedReader.readLine();
+
+        int res = Result.makeAnagram(a, b);
+
+        bufferedWriter.write(String.valueOf(res));
         bufferedWriter.newLine();
+
         bufferedReader.close();
         bufferedWriter.close();
     }
 }
 
 
+class Result {
+
+    /*
+     * Complete the 'makeAnagram' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING a
+     *  2. STRING b
+     */
+
+    public static int makeAnagram(String a, String b) {
+        // Write your code here
+
+        int [] aCount = new int [26];
+        int [] bCount = new int [26];// a.length = b.length
+        int res = 0;
+
+        for(int i = 0; i < a.length(); i++) {
+            aCount[a.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < b.length(); i++) {
+            bCount[b.charAt(i) - 'a']++;
+        }
+
+
+        for(int i = 0; i < 26; i++) {
+            res += Math.abs(aCount[i] - bCount[i]);
+        }
+
+        return res;
+    }
+
+}
